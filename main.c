2cb3664c00fb7156457f89b161aa78ba14f93d43
yannb <blanc.yann.1@gmail.com>
2021-03-28 18:48:09 +0200

    exo 1 et 2

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void my_swap(int *a, int *b);
+
+
+int main()
+{
+    int *p1 = NULL, *p2 = NULL;
+
+    print
+
+    //my_swap(p1 , p2);
+    return 0;
+}
+
+void my_swap(int *a, int *b)
+{
+
+}
